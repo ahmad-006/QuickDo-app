27e314bbc12030a23a6fc2facab9de37d6552b64
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-19 23:31:18 +0500

    feat: implented last section section on homepage

---
+++ frontend/src/components/LastSection.jsx
@@ -0,0 +1,13 @@
+function LastSection() {
+  return (
+    <main className="mt-8 md:py-24 py-20 bg-[#00835A] text-center p-4 flex flex-col gap-6 justify-center items-center">
+      <p>Ready to get organized?</p>
+      <p>Join thousands of users who've simplified their task management.</p>
+      <button className="px-4 py-3 shadow-xl bg-gray-100 text-[#00835a] rounded-xl cursor-pointer font-medium ">
+        Start Free Today
+      </button>
+    </main>
+  );
+}
+
+export default LastSection;
